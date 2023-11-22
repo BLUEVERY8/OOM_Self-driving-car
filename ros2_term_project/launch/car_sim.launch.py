a16947331ad4b1e7a0004fc0530d1c415b9233f2
yeonggeunLee <dldudrms506@gmail.com>
2023-11-22 01:21:07 -0800

    언덕구간 전체 완료

---
+++ ros2_term_project/launch/car_sim.launch.py
@@ -0,0 +1,86 @@
+#!/usr/bin/env python3
+#
+# Copyright 2023. Prof. Jong Min Lee @ Dong-eui University
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+import os
+import sys
+from ament_index_python.packages import get_package_share_directory
+from launch import LaunchDescription
+from launch.actions import DeclareLaunchArgument
+from launch_ros.actions import Node
+from launch.actions import ExecuteProcess
+import json
+from launch.substitutions import LaunchConfiguration
+
+
+print(os.path.realpath(__file__))
+
+ld = LaunchDescription()
+car = 'PR001'
+
+def generate_launch_description():
+    # configuration
+    world = LaunchConfiguration('world')
+    print('world =', world)
+    world_file_name = 'test_track.world'
+    world = os.path.join(get_package_share_directory('ros2_term_project'),
+                         'worlds', world_file_name)
+    print('world file name = %s' % world)
+
+
+    # ld = LaunchDescription()
+    declare_argument = DeclareLaunchArgument(
+        'use_sim_time',
+        default_value='true',
+        description='Use simulation (Gazebo) clock if true')
+
+    gazebo_run = ExecuteProcess(
+        cmd=['gazebo', '-s', 'libgazebo_ros_factory.so', world],
+        output='screen')
+
+    starter = Node(
+        package='ros2_term_project',
+        executable='starter',
+        name='starter',
+        arguments=[car],
+        output='screen'
+    )
+
+    controller = Node(
+        package='ros2_term_project',
+        executable='controller',
+        name='controller',
+        output='screen'
+    )
+
+    line_follower = Node(
+        package='ros2_term_project',
+        executable='line_follower',
+        name='line_follower',
+        arguments=[car],
+        output='screen'
+    )
+
+    ld.add_action(declare_argument)
+    ld.add_action(gazebo_run)
+    ld.add_action(controller)
+    ld.add_action(line_follower)
+    ld.add_action(starter)
+
+
+    # spawn prius_hybrid
+
+    return ld
---
+++ ros2_term_project/ros2_term_project/controller.py
@@ -0,0 +1,89 @@
+import rclpy
+from rclpy.node import Node
+import time
+from custom_interface.msg import Target
+from geometry_msgs.msg import Twist
+from std_msgs.msg import String
+
+class Controller(Node):
+
+    def __init__(self):
+        super().__init__('controller')
+        self.subscription = self.create_subscription(
+            Target,
+            'start_car',
+            self.listener_callback,
+            10)
+        self.state_subscription = None
+        self.issue_subscription = None
+        self.publisher_ = None
+        self.stop = False
+        self.turn = False
+
+    def listener_callback(self, msg):
+        car = msg.car
+        self.get_logger().info('I heard: "%s"' % msg.car)
+        self.publisher_ = self.create_publisher(Twist, 'demo/' + car + '_cmd_demo', 10)
+        twist = Twist()
+        twist.linear.x = 6.0
+        self.publisher_.publish(twist)
+        time.sleep(3)
+        self.state_subscription = self.create_subscription(Twist, 'state_update', self.state_listener_callback, 10)
+        self.stop_subscription = self.create_subscription(String, 'stop_issue_update', self.stop_listener_callback, 10)
+        self.end_subscription = self.create_subscription(String, 'end_issue_update', self.end_listener_callback, 10)
+
+    def state_listener_callback(self, delta: Twist):
+        if not self.stop:
+            msg = Twist()
+            self.get_logger().info('I heard %f' % delta.angular.z)
+            msg.angular.z = delta.angular.z
+            if delta.angular.z > 0.02:
+                self.turn = True
+                msg.linear.x = 3.0
+            else:
+                self.turn = False
+                msg.linear.x = 6.0
+            self.publisher_.publish(msg)
+
+    def stop_listener_callback(self, issue: String):
+        msg = Twist()
+
+        if not self.turn and issue.data == '정지':
+                self.stop = True
+                msg.linear.x = 1.0
+                msg.angular.z = 0.0
+    
+                self.publisher_.publish(msg)
+                time.sleep(3)
+                self.stop = False   
+
+    def end_listener_callback(self, issue: String):
+        msg = Twist()
+
+        if issue.data == '종료':
+            self.stop = True
+            msg.linear.x = 0.0
+            msg.angular.z = 0.0
+
+            self.publisher_.publish(msg)
+            self.destroy_node()
+            return
+
+
+
+def main(args=None):
+    rclpy.init(args=args)
+
+    controller = Controller()
+
+    rclpy.spin(controller)
+
+    # Destroy the node explicitly
+    # (optional - otherwise it will be done automatically
+    # when the garbage collector destroys the node object)
+    controller.destroy_node()
+    rclpy.shutdown()
+
+
+if __name__ == '__main__':
+    main()
